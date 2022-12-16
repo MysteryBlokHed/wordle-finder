f5f5f824c540ee533ce60eb0e15e9221bdcf79c0
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-12-16 13:49:45 -0500

    feat: Add option to delete wordlists

--- src/lib/Wordlists.svelte
+++ src/lib/Wordlists.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import Button from '@smui/button'
+  import Button, { Label } from '@smui/button'
+  import Checkbox from '@smui/checkbox'
   import DataTable, { Head, Body, Row, Cell } from '@smui/data-table'
   import { DatePicker } from 'date-picker-svelte'
   import METHODS from '../seed-methods'
@@ -17,23 +18,37 @@
 
   listsStore.subscribe(value => (lists = value))
 
-  const findCurrentWord = (date: Date, list: string): string => {
-    if (!(list in lists)) throw new TypeError(`List ${list} not found`)
-    const listObj = lists[list]
-    if (!(listObj.method in METHODS))
-      throw new TypeError(`Unknown method ${listObj.method} specified`)
+  const findCurrentWord = (date: Date, list: List): string => {
+    if (!(list.method in METHODS))
+      throw new TypeError(`Unknown method ${list.method} specified`)
 
-    const index = METHODS[listObj.method].method(date, listObj)
-    if (!(index in lists[list].list)) return '(Not Found)'
+    const index = METHODS[list.method].method(date, list)
+    if (!(index in list.list)) return '(Not Found)'
 
-    return lists[list].list[index].toUpperCase()
+    return list.list[index].toUpperCase()
+  }
+
+  let options: Array<{ name: string; word: string }>
+  $: options = Object.entries(lists).map(([key, value]) => ({
+    name: key,
+    word: findCurrentWord(wordDate, value),
+  }))
+  let selected = []
+
+  const deleteSelected = () => {
+    for (const option of selected) {
+      delete lists[option.name]
+      options.splice(options.indexOf(option), 1)
+    }
+    selected.length = 0
+    listsStore.set(lists)
   }
 
   window.lists = lists
 </script>
 
 <Button on:click={() => (pickerShown = !pickerShown)} variant="raised">
-  Select Date For Word
+  <Label>Select Date For Word</Label>
 </Button>
 {#if pickerShown}
   <br />
@@ -49,16 +64,36 @@
 <DataTable table$aria-label="List of wordlists">
   <Head>
     <Row>
+      <Cell checkbox>
+        <Checkbox />
+      </Cell>
       <Cell>List Name</Cell>
       <Cell>Word</Cell>
     </Row>
   </Head>
   <Body>
-    {#each Object.keys(lists) as name}
+    {#each options as option}
       <Row>
-        <Cell>{name}</Cell>
-        <Cell>{findCurrentWord(wordDate, name)}</Cell>
+        <Cell checkbox>
+          <Checkbox
+            bind:group={selected}
+            checked={selected.includes(option)}
+            value={option}
+            valueKey={option.name}
+          />
+        </Cell>
+        <Cell>{option.name}</Cell>
+        <Cell>{option.word}</Cell>
       </Row>
     {/each}
   </Body>
 </DataTable>
+{#if selected.length}
+  <br />
+  <Button on:click={deleteSelected} variant="raised" color="secondary">
+    <Label>
+      Delete {selected.length}
+      {selected.length === 1 ? 'list' : 'lists'}
+    </Label>
+  </Button>
+{/if}
