75a2c23a83901746dffc951f0f5881edbc2dc87e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-12-17 13:46:11 -0500

    feat: Add link to GitLab repo on site

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+  import Button, { Icon, Label } from '@smui/button'
+
   import Wordlists from './lib/Wordlists.svelte'
   import CreateWordlist from './lib/CreateWordlist.svelte'
   import HelpModal from './lib/HelpModal.svelte'
 </script>
 
 <main>
-  <h1>Wordle Finder</h1>
+  <h1 style="margin-bottom: 0;">Wordle Finder</h1>
   <h2>Find the current word for any given wordlist</h2>
   <Wordlists />
   <br />
@@ -13,4 +15,16 @@
   <CreateWordlist />
   <br />
   <HelpModal />
+  <br />
+  <br />
+  <Button
+    variant="raised"
+    color="secondary"
+    href="//gitlab.com/MysteryBlokHed/wordle-finder"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <Label>Source</Label>
+    <Icon class="material-icons">open_in_new</Icon>
+  </Button>
 </main>
