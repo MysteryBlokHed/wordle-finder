956b5acba792bcbdc4c054359a7b575a776048eb
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-12-16 19:53:32 -0500

    feat: Update help menu to mention Wordle and rearrange sections

--- src/lib/HelpModal.svelte
+++ src/lib/HelpModal.svelte
@@ -13,6 +13,27 @@
 >
   <Title id="help-modal-title">Help</Title>
   <Content id="help-modal-content">
+    <h2>Wordlist methods</h2>
+    <p>
+      Some Wordle clones will have wordlists that simply map the current day to
+      an index in the list, i.e. day 1 is index 5, day 2 is index 6, and so on.
+      However, there are others which use a more complex system to select their
+      words. The site has some of these methods built-in, which can be selected
+      from the dropdown box in the "Add Wordlist" menu.
+    </p>
+    <p>The original Wordle has a method included in the dropdown.</p>
+    <p>
+      Another site that has its method included is a French wordle clone, <a
+        href="https://wordle.louan.me/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        wordle.louan.me.
+      </a>
+      If you're adding this site's wordlist, simply select its method name from the
+      dropdown box before saving.
+    </p>
+
     <h2>Finding a wordlist</h2>
     <p>
       For most Wordle clones, the wordlist will be an array somewhere in the
@@ -27,30 +48,14 @@
       <code>bundle.js</code>
     </p>
     <p>
+      The original Wordle doesn't require a wordlist to be provided due to its
+      implementation.
+    </p>
+    <p>
       Most sites will have two lists: a list of valid words, and a list of
       actual answers. You can often tell the difference pretty easily because
       the list of actual answers will be in a random order, and the list of
       valid words will be alphabetical. Copy the list of actual answers.
-    </p>
-
-    <h2>Wordlist methods</h2>
-    <p>
-      Some Wordle clones will have wordlists that simply map the current day to
-      an index in the list, i.e. day 1 is index 5, day 2 is index 6, and so on.
-      However, there are others which use a more complex system to select their
-      words. The site has some of these methods built-in, which can be selected
-      from the dropdown box in the "Add Wordlist" menu.
-    </p>
-    <p>
-      One site that has its method included is a French wordle clone, <a
-        href="https://wordle.louan.me/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        wordle.louan.me.
-      </a>
-      If you're adding this site's wordlist, simply select its method name from the
-      dropdown box before saving.
     </p>
   </Content>
   <Actions>
