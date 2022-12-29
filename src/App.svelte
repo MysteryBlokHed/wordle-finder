b09c0dc22934192f8ed1f180b5f2e1c7b7b29ae9
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-12-28 22:20:04 -0500

    feat: Add license header to source files

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,20 @@
+<!--
+Wordle Finder - A site to add generic wordlists for Wordle-like games
+and find solutions for a given date.
+Copyright (C) 2022  Adam Thompson-Sharpe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <script lang="ts">
   import Button, { Icon, Label } from '@smui/button'
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,20 @@
+/**
+ * Wordle Finder - A site to add generic wordlists for Wordle-like games
+ * and find solutions for a given date.
+ * Copyright (C) 2022  Adam Thompson-Sharpe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import type { Method } from './index-methods'
 
 export const LIST_TIMEZONES = ['UTC', 'Local'] as const
