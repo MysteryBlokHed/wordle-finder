bee98c7ecc4a8ac605d51b03c1271a3d5c670c1d
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2023-01-03 14:11:46 -0500

    feat: Update list compression script

--- compress.js
+++ compress.js
@@ -1,5 +1,5 @@
 import process from 'node:process'
-import { readFileSync } from 'node:fs'
+import { readFileSync, writeFileSync } from 'node:fs'
 import LZString from 'lz-string'
 
 const args = process.argv.slice(2)
@@ -18,3 +18,11 @@ const compressed = LZString.compressToBase64(JSON.stringify(list))
 
 console.log('Compressed:')
 console.log(compressed)
+console.log('Original length:', JSON.stringify(list).length)
+console.log('Length:', compressed.length)
+
+const compressedPath = `${args[0]}.compressed`
+
+writeFileSync(compressedPath, compressed)
+
+console.log('Saved to', compressedPath)
